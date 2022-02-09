9509b17b76d792c2806b19bfed6d4360e54b079e
Yida <yidachen30@yahoo.com.au>
2022-02-09 20:42:18 +1100

    hitsound

--- src/components/AimGame.js
+++ src/components/AimGame.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect, useCallback } from 'react'
 import './components.css'
 import './tablestyling.css'
-import DanielScream from '../sounds/R2DC.mp3'
+import hitding from '../sounds/Hitsound.wav'
 
 export const AimGame = () => {
 
@@ -18,16 +18,37 @@ const Timer = () => {
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0)
 
+  const [displayMinute, setDisplayMinute] = useState("00");
+  const [displaySecond, setDisplaySecond] = useState("00");
+
+  //wrap interval in useeffect so that it only creates one instead of an interval everytime the page rerenders.
   useEffect(() => {
     const timerInterval = setInterval(() => {
       const currSecond = second + 1;
 
       if (currSecond > 60) {
         setSecond(0)
+        setDisplaySecond("00")
         setMinute((minute) => minute + 1)
+
+        if (minute++ < 10) {
+          setDisplayMinute("0" + minute++)
+        }
+        else {
+          setDisplayMinute(minute++)
+        }
+
       }
       else {
-          setSecond(currSecond)
+        setSecond(currSecond)
+
+        if (currSecond < 10) {
+          setDisplaySecond("0" + currSecond)
+        }
+        else {
+          setDisplaySecond(currSecond)
+        }
+
       }
       
       console.log(second)
@@ -41,16 +62,18 @@ const Timer = () => {
 
   
   return(
-    <div>
-      {minute} : {second}
-    </div>
+    <h2 style={{textAlign: 'center'}}>
+      {displayMinute} : {displaySecond}
+    </h2>
   )
 }
 
 const AimArea = () => {
 
-  const [targetCoordinate, setTargetCoordinate] = useState([48,45])
+  const [hitsound, setHitsound] = useState(new Audio(hitding))
+  const [targetCoordinate, setTargetCoordinate] = useState([49,45])
 
+  hitsound.volume = 0.2
   const generateTarget = () => {
     let newCoordinate = [Math.floor(Math.random() * 90), Math.floor(Math.random() * 90)]
 
@@ -61,6 +84,7 @@ const AimArea = () => {
 
       <div style={{marginLeft: targetCoordinate[0] + 'vw', marginTop: targetCoordinate[1] + 'vh'}} className='target' onClick={() => {
         generateTarget()
+        hitsound.play()
       }}>
         <div style={{height: '100%', borderLeft: '1px solid black', marginLeft:'50%'}}>
         </div>
